3be7d10b39c41bd2c4a47f411c5bf9b9b0dd34e8
Regina Giniyatova <giniyatovar@yandex.ru>
2023-07-11 22:34:07 +0300

    move crash test

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,6 +4,12 @@ const { Joi, celebrate } = require('celebrate');
 const { createUser, login } = require('../controllers/auth');
 const { URL_REGEX } = require('../utils/constants');
 
+auth.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 auth.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
